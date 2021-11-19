322350ac8762546f7f67cf6bc91264e30b9f5627
Chaimaa ET-Toumy <81262223+chaima-proge@users.noreply.github.com>
2021-11-19 21:50:15 +0100

    challenge 10

---
+++ challenge des boucles/exercice10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n,s=0,m=0,i;
+    for(i=0;i<=10;i++)
+    {
+        printf("entrer un nombre : ");
+        scanf("%d",&n);
+        if(n<0 || n>10)
+        printf("entrer un nombre entre 0 et 10");
+        else if(n>m)
+        {
+            m = n;
+        }
+            s = s+n;
+    }
+     printf("la somme est %d:",s);
+     printf("le max est %d:",m);
+  
+     return 0;
+}
